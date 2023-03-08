f41c2a02f050a6835630d0b3816eeaa69711f275
Dave Bratby <dbratby1992@gmail.com>
2023-03-08 07:24:34 +0000

    Completed the FizzBuzz Challenge

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,17 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+  const dividesBy3 = (number % 3) === 0;
+  const dividesBy5 = (number % 5) === 0;
 
+  if (dividesBy3 && dividesBy5) {
+      return 'FizzBuzz'
+  }
+  if (dividesBy3){
+      return 'Fizz'
+  }
+  if (dividesBy5) {
+      return 'Buzz'
+  }
+  return number 
+};
+    
 module.exports = fizzBuzz;
