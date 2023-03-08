8a538dd3f76ee62e0449152c3e4131b632ddf8db
Dave Bratby <dbratby1992@gmail.com>
2023-03-08 10:20:27 +0000

    completed the catDogYears test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,21 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+   let humanYears = number;
+   let catYears = 0;
+   let dogYears = 0;
+   for (let i = 0; i < humanYears; i++){
+       if (i === 1){
+           catYears += 15
+           dogYears += 15;
+       }else if(i === 2){
+               catYears += 9
+               dogYears += 9
+           }else{
+                   catYears += 4
+                   dogYears += 5
+               }
+           }
+           return [humanYears, catYears, dogYears];
+       }
+   
 
-module.exports = humanCatDogYears;
+       module.exports = humanCatDogYears;
