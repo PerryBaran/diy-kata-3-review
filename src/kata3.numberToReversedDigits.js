1b9040ec24ae58fa52465c5d6d16fa5ae5ed50e5
Matt Mullins <matt.mullins@live.com>
2023-03-11 19:21:28 +0000

    Up to Number Reversed Draft

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,31 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+    // const digits = [];
+
+    // const addToArray = (number,digits) => {
+    // digits.push(number);
+    // };
+
+    // return digits
+
+    // digits.join(',');
+
+        // const arr = [];
+        // arr.push(number);
+        // arr.join(',');
+        // arr.reverse();
+      
+// };
+
+// const numberToReversedDigits = (number) => {
+//     const arr = [];
+//     arr.push(number);
+//     arr.join(',');
+
+const arr = [];
+for (let i = 0; i < number.length; i++) {
+  arr.push(number[i]);
+}
+return arr.reverse().join(',');
+};
 
 module.exports = numberToReversedDigits;
