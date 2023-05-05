3a42eaabeb1899a0ed044f5523d23e4293344217
u1986f <u1986f@gmail.com>
2023-05-05 12:51:37 +0100

    things that weren't committed

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,19 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "Harry",
+        },
+        {
+          name: "Ron",
+        },
+        {
+          name: "Hermione",
+        },
+      ])
+    ).toBe("Harry, Ron & Hermione");
+  });
 });
