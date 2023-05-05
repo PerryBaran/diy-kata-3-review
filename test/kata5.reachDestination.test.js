1447b9b2313bf3a13e629850583701884c9ffab7
Cristina Soare <kiti.soare@ymail.com>
2023-05-05 17:34:43 +0100

    passed fifth kata

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,8 +1,10 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  it("returns string with estimated time of arrival", () => {
+  test("returns string with estimated time of arrival", () => {
     expect(reachDestination(44,10)).toBe('I should be there in 4.5 hours.')
+    expect(reachDestination(30,7)).toBe('I should be there in 4.5 hours.')
+    expect(reachDestination(20,10)).toBe('I should be there in 2 hours.')
 
   });
 });
