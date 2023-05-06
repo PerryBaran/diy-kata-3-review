f2ee98b44c0539b015d5926721597196ee9a9412
Cristina Soare <kiti.soare@ymail.com>
2023-05-06 16:15:47 +0100

    changed booleanToWord from if function to a ternary operator

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,8 +1,5 @@
 const booleanToWord = (boolean) => {
-  if (boolean === true) {
-    return "Yes";
-  }
-  return "No";
+  return boolean === true ? "Yes" : "No";
 };
 
 module.exports = booleanToWord;
