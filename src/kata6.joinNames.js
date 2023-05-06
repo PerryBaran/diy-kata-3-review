9dc883108f1e9783f6b3542098e3a42d3419cf1f
Cristina Soare <kiti.soare@ymail.com>
2023-05-06 13:44:14 +0100

    passed joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  let concatenatedNames = "";
+  for (let i = 0; i < namesObj.length; i++) {
+    if (i === namesObj.length - 1) {
+      concatenatedNames += " & " + namesObj[i].name;
+    } else if ( i === namesObj.length - 2) {
+      concatenatedNames += namesObj[i].name;
+    } else {
+      concatenatedNames += namesObj[i].name + ', ';
+    }
+  }
+  return concatenatedNames;
+}
 
 module.exports = joinNames;
