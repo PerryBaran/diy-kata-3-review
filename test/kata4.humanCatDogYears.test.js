2309e0aebc1d4ed8e3f1fa9f87d2c00cae18c85f
Chris O'Neill <chris_oslow@hotmail.com>
2023-11-02 18:43:37 +0000

    TDD Kata, Human, Cat, Dog

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("turns human years into cat and dog years", () => {
+    expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+  });
+});
