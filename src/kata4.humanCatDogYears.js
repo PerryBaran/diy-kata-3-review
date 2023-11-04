40bb5d723cf4750ace65cae68f01c9e9d5fe7ae2
Ashley Daniels <ashdaniels25gmail.com>
2023-11-04 15:03:04 +0000

    humanCatDogYears Complete

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,19 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  if (number === 1) {
+    return [15, 15, 15];
+  }
+
+  if (number === 2) {
+    return [24, 24, 24];
+  }
+
+  if (number > 2) {
+    const catYears = 24 + (number - 2) * 4;
+    const dogYears = 24 + (number - 2) * 5;
+    return [number * 15, catYears, dogYears];
+  }
+
+  throw new Error("Input is not a valid age");
+};
 
 module.exports = humanCatDogYears;
