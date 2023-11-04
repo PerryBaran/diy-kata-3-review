08d0da886d3c003d0a4a9d68434ed6cb716c7479
Ashley Daniels <ashdaniels25gmail.com>
2023-11-04 15:07:23 +0000

    reach Destination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,14 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  if (speed === 0) {
+    return "You are not moving, so you're already there!";
+  }
+
+  const time = distance / speed;
+  if (time < 1) {
+    const minutes = Math.round(time * 60);
+    return `You will arrive in ${minutes} minute${minutes > 1 ? "s" : ""}.`;
+  }
+  return `You will arrive in ${time} hour${time > 1 ? "s" : ""}.`;
+};
 
 module.exports = reachDestination;
