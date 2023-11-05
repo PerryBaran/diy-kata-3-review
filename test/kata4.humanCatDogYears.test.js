b7c68de513f4ada6429da60e525f3ea9cbf1ebad
James Carrier <jic191278@gmail.com>
2023-11-05 16:22:56 +0000

    complete humanCatDogYears

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
-const { humanCatDogYears } = require("../src");
+const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+	test('check number inputs', () => {
+		expect(humanCatDogYears(0)).toStrictEqual([0, 0, 0]);
+		expect(humanCatDogYears(1)).toStrictEqual([1, 15, 15]);
+		expect(humanCatDogYears(2)).toStrictEqual([2, 24, 24]);
+		expect(humanCatDogYears(3)).toStrictEqual([3, 28, 29]);
+		expect(humanCatDogYears(4)).toStrictEqual([4, 32, 34]);
+	});
+});
