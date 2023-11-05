8910042b3f57298b22fb53243ce6f58da07fecc4
Ashley Daniels <ashdaniels25gmail.com>
2023-11-05 12:38:01 +0000

    Reach destination finished

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,14 +1,6 @@
 const reachDestination = (distance, speed) => {
-  if (speed === 0) {
-    return "You are not moving, so you're already there!";
-  }
-
-  const time = distance / speed;
-  if (time < 1) {
-    const minutes = Math.round(time * 60);
-    return `You will arrive in ${minutes} minute${minutes > 1 ? "s" : ""}.`;
-  }
-  return `You will arrive in ${time} hour${time > 1 ? "s" : ""}.`;
+  const arrivalTime = Math.ceil((distance / speed) * 2) / 2;
+  return `I will be there in ${arrivalTime}, see you soon.`;
 };
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -2,20 +2,8 @@ const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
-    // Test with a distance of 120 miles and a speed of 60 mph
-    const result1 = reachDestination(120, 60);
-    expect(result1).toBe("You will arrive in 2 hours.");
-
-    // Test with a distance of 90 miles and a speed of 45 mph
-    const result2 = reachDestination(90, 45);
-    expect(result2).toBe("You will arrive in 2 hours.");
-
-    // Test with a distance of 100 miles and a speed of 0 mph (stopped)
-    const result3 = reachDestination(100, 0);
-    expect(result3).toBe("You are not moving, so you're already there!");
-
-    // Test with a distance of 30 miles and a speed of 90 mph
-    const result4 = reachDestination(30, 90);
-    expect(result4).toBe("You will arrive in 20 minutes.");
+    expect(reachDestination(44, 10)).toEqual(
+      "I will be there in 4.5, see you soon."
+    );
   });
 });
