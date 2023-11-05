dc6523b2644737c69fc5aa1152649127a2736ef1
James Carrier <jic191278@gmail.com>
2023-11-05 15:40:11 +0000

    complete booleanToWord and numberToReversedDigits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+	return number
+		.toString()
+		.split('')
+		.reverse()
+		.map((item) => Number(item));
+};
 
 module.exports = numberToReversedDigits;
